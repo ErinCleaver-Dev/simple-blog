2f7cd7c15a70c6255664585b5fb86c6449f45d93
ecleaver <erincleaver@grcc.email.edu>
2021-08-03 20:15:30 -0400

    update

--- src/Components/Posts/Post/Post.js
+++ src/Components/Posts/Post/Post.js
@@ -1,8 +1,11 @@
+import Parser from 'html-react-parser';
 
-
-const Post = () => {
+const Post = (props) => {
     return (
         <div>
+            <div className="post_information"><p>author: {props.author}</p><p>{props.data}</p><p>{props.section}</p></div>
+            <div className="post_title">{props.title}</div>
+            <div className="post">{Parser(props.post)}</div>
         </div>
     )
 }
--- src/Components/Posts/Post/Post_Summary.js
+++ src/Components/Posts/Post/Post_Summary.js
@@ -1,10 +1,10 @@
 const Post_Summary = (props) => {
     return (
-        <section className="post_summary">
+        <div onClick={props.loaded} className="post_summary">
             <div className="post_information"><a>{props.author}</a><p>{props.date}</p><a>{props.section}</a></div>
             <div><h2>{props.title}</h2></div>
             <div><p>{props.summary}</p></div>
-        </section>
+        </div>
     )
 }
 
